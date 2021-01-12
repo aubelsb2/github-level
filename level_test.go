3dc345c248224921db57467848ba499c7c03cf38
arran <arran4@gmail.com>
2021-01-12 21:43:14 +1100

    Level 1 test added.

---
+++ level_test.go
@@ -0,0 +1,54 @@
+package github_level
+
+import (
+	"log"
+	"testing"
+	"time"
+)
+
+func TestL1(t *testing.T) {
+	for _, each := range []struct {
+		Name  string
+		Level GithubLevelV1
+	}{
+		{
+			Name: "arran4",
+			Level: GithubLevelV1{
+				SumLastUpdatedDays:    406628,
+				TotalReposWithUpdated: 23,
+				PublicRepos:           30,
+				ForkedRepos:           17,
+				ArchivedRepos:         7,
+				LicenseSophistication: 0,
+				Licenses: map[string]int{
+					"": 30,
+				},
+				FirstPublicRepoDate:   TimeMustParse(time.Parse(time.RFC3339, "2010-01-31T04:07:52Z")),
+				LargestForkCount:      22,
+				LargestStargazerCount: 49,
+				LargestWatcherCount:   49,
+				SelfNamedRepo:         false,
+				UserCreatedDate:       TimeMustParse(time.Parse(time.RFC3339, "2009-08-04T03:16:20Z")),
+				Followers:             17,
+				Following:             34,
+				OwnedPrivateRepos:     0,
+				PublicGists:           62,
+				PRRequests:            3,
+				IssuesLogged:          6,
+			},
+		},
+	} {
+		t.Run(each.Name, func(t *testing.T) {
+			v := each.Level.Calculate()
+			if v > 1000 || v < 10 {
+				t.Fatalf("Got level 1 level: %v", v)
+			}
+			t.Logf("Got level 1 level: %v", v)
+		})
+	}
+}
+
+func TimeMustParse(parse time.Time, err error) time.Time {
+	log.Printf("ERROR: %v", err)
+	return parse
+}
