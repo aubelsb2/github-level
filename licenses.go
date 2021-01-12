0333901d07c2913be3432021c1553a3be651edbf
arran <arran4@gmail.com>
2021-01-12 20:55:34 +1100

    Initial

---
+++ licenses.go
@@ -0,0 +1,43 @@
+package github_level
+
+var (
+	// Just some fun - based on the number of clauses I thought that it ought to have when I wrote this
+	// Really I have no idea about most of these.
+	LicenseSophistication = map[string]int{
+		"":                   0,
+		"afl-3.0":            8,
+		"apache-2.0":         6,
+		"artistic-2.0":       2,
+		"bsl-1.0":            4,
+		"bsd-2-clause":       2,
+		"bsd-3-clause":       3,
+		"bsd-3-clause-clear": 3,
+		"cc":                 3,
+		"cc0-1.0":            1,
+		"cc-by-4.0":          4,
+		"cc-by-sa-4.0":       3,
+		"wtfpl":              1,
+		"ecl-2.0":            6,
+		"epl-1.0":            6,
+		"epl-2.0":            6,
+		"eupl-1.1":           5,
+		"agpl-3.0":           5,
+		"gpl":                7,
+		"gpl-2.0":            7,
+		"gpl-3.0":            7,
+		"lgpl":               8,
+		"lgpl-2.1":           8,
+		"lgpl-3.0":           8,
+		"isc":                1,
+		"lppl-1.3c":          3,
+		"ms-pl":              4,
+		"mit":                3,
+		"mpl-2.0":            4,
+		"osl-3.0":            4,
+		"postgresql":         3,
+		"ofl-1.1":            3,
+		"ncsa":               3,
+		"unlicense":          0,
+		"zlib":               4,
+	}
+)
--- stats.go
+++ stats.go
@@ -5,16 +5,27 @@ import (
 	"github.com/google/go-github/github"
 	"log"
 	"os"
+	"time"
 )
 
 type Stats struct {
-	PublicRepos int
-	Licenses    map[string]int
+	SumLastUpdatedDays    int64
+	TotalReposWithUpdated int
+	PublicRepos           int
+	ForkedRepos           int
+	ArchivedRepos         int
+	LicenseSophistication int
+	Licenses              map[string]int
+	FirstPublicRepoDate   time.Time
+	LargestForkCount      int
+	LargestStargazerCount int
+	LargestWatcherCount   int
 }
 
 func GetStats(ctx context.Context) (stats *Stats) {
 	stats = &Stats{
-		Licenses: map[string]int{},
+		Licenses:            map[string]int{},
+		FirstPublicRepoDate: time.Now(), // Born yesterday - https://open.spotify.com/track/22pzlAb4SynBW4aO0HCwo1?si=VvB1IcC3Q_6PwS6-pYR9tA
 	}
 	client := github.NewClient(nil)
 
@@ -33,6 +44,33 @@ func GetStats(ctx context.Context) (stats *Stats) {
 		}
 		stats.PublicRepos++
 		stats.Licenses[PS(repository.LicenseTemplate)]++
+		if stats.Licenses[PS(repository.LicenseTemplate)] == 1 {
+			if v, ok := LicenseSophistication[PS(repository.LicenseTemplate)]; ok {
+				stats.LicenseSophistication += v
+			}
+		}
+		if PB(repository.Fork) {
+			stats.ForkedRepos++
+		}
+		if PB(repository.Archived) {
+			stats.ArchivedRepos++
+		}
+		if repository.CreatedAt != nil && repository.CreatedAt.Before(stats.FirstPublicRepoDate) {
+			stats.FirstPublicRepoDate = repository.CreatedAt.Time
+		}
+		if repository.ForksCount != nil && PI(repository.ForksCount) > stats.LargestForkCount {
+			stats.LargestForkCount = *repository.ForksCount
+		}
+		if repository.StargazersCount != nil && PI(repository.StargazersCount) > stats.LargestStargazerCount {
+			stats.LargestStargazerCount = *repository.StargazersCount
+		}
+		if repository.WatchersCount != nil && PI(repository.WatchersCount) > stats.LargestWatcherCount {
+			stats.LargestWatcherCount = *repository.WatchersCount
+		}
+		if repository.UpdatedAt != nil && !PB(repository.Archived) {
+			stats.SumLastUpdatedDays += repository.UpdatedAt.Time.Unix() / 60 / 60 / 24
+			stats.TotalReposWithUpdated++
+		}
 	}
 	return
 }
@@ -46,4 +84,10 @@ func PS(name *string) string {
 		return *name
 	}
 	return ""
+}
+func PI(name *int) int {
+	if name != nil {
+		return *name
+	}
+	return 0
 }
