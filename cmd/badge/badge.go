4154542e6b200e49ed20d76fdfb7a62d3d34e3d4
arran <arran4@gmail.com>
2021-01-12 21:47:18 +1100

    CLI and git action component done

---
+++ cmd/badge/badge.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	github_level "github.com/arran4/github-level"
+	"image/png"
+	"log"
+	"os"
+)
+
+func main() {
+	ctx := context.Background()
+
+	stats := github_level.GetStats(ctx)
+
+	for _, l := range []github_level.Level{
+		stats.V1(),
+	} {
+		i := l.Badge()
+		w, err := os.Create(fmt.Sprintf("badge-version-%v.png", l.Version()))
+		if err != nil {
+			log.Panicf("OS Create error: %v", err)
+		}
+		if err := png.Encode(w, i); err != nil {
+			log.Panicf("PNG encode error: %v", err)
+		}
+		if err := w.Close(); err != nil {
+			log.Panicf("Writer close error: %v", err)
+		}
+	}
+
+}
